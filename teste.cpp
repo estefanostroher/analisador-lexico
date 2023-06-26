aeedbc496c5d80c443bbcf2afdf0fcaa3b551eca
Lucas Santos Oliveira <91764720+LucasSantosOliveira2@users.noreply.github.com>
2023-06-26 19:50:39 -0300

    Update teste.cpp

--- teste.cpp
+++ teste.cpp
@@ -1,90 +1,63 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 enum Token {
-    IF,
+    IF = 256,
     THEN,
+    DO,
     WHILE,
     FOR,
     ELSE,
+    SWITCH,
     ID,
     NUM,
-    SOMA
+    SOMA,
+    INT,
+    BOOL,
+    ATRIBUICAO
 };
 
-Token getNextToken(const std::string& input){
-    static int currentPosition = 0;
-    static std::string currentToken = "";
-
-    // Verifica se a posição atual está além do comprimento da entrada
-    if (currentPosition >= input.length()) {
-        // Retorna um token de fim de arquivo
-        return Token::EOF;
+Token getToken(const std::string& tokenString) {
+    if (tokenString == "if") {
+        return IF;
+    } else if (tokenString == "then") {
+        return THEN;
+    } else if (tokenString == "do") {
+        return DO;
+    } else if (tokenString == "while") {
+        return WHILE;
+    } else if (tokenString == "for") {
+        return FOR;
+    } else if (tokenString == "else") {
+        return ELSE;
+    } else if (tokenString == "switch") {
+        return SWITCH;
+    } else if (tokenString == "ID") {
+        return ID;
+    } else if (tokenString == "NUM") {
+        return NUM;
+    } else if (tokenString == "+") {
+        return SOMA;
+    } else if (tokenString == "int") {
+        return INT;
+    } else if (tokenString == "bool") {
+        return BOOL;
+    } else if (tokenString == "=") {
+        return ATRIBUICAO;
+    } else {
+        // Retornar um valor especial para indicar que o token não foi encontrado
+        return ID; // ou qualquer outro valor que faça sentido no seu programa
     }
-
-    // Pula espaços em branco
-    while (input[currentPosition] == ' ' || input[currentPosition] == '\t') {
-        currentPosition++;
-    }
-
-    // Verifica os caracteres especiais
-    switch (input[currentPosition]) {
-        case '+':
-            currentPosition++;
-            return Token::SOMA;
-        // Adicione mais casos para outros caracteres especiais, se necessário
-
-        default:
-            break;
-    }
-
-    // Verifica se é um número
-    if (isdigit(input[currentPosition])) {
-        currentToken = "";
-        while (isdigit(input[currentPosition])) {
-            currentToken += input[currentPosition];
-            currentPosition++;
-        }
-        return Token::NUM;
-    }
-
-    // Verifica se é uma palavra-chave ou identificador
-    if (isalpha(input[currentPosition])) {
-        currentToken = "";
-        while (isalnum(input[currentPosition])) {
-            currentToken += input[currentPosition];
-            currentPosition++;
-        }
-
-        // Verifica se é uma palavra-chave conhecida
-        if (currentToken == "if") {
-            return Token::IF;
-        } else if (currentToken == "then") {
-            return Token::THEN;
-        } else if (currentToken == "while") {
-            return Token::WHILE;
-        } else if (currentToken == "for") {
-            return Token::FOR;
-        } else if (currentToken == "else") {
-            return Token::ELSE;
-        }
-
-        // Se não for uma palavra-chave, é um identificador
-        return Token::ID;
-    }
-
-    // Caso contrário, é um caractere inválido
-    currentPosition++;
-    return Token::INVALID;
 }
 
 int main() {
-    std::string input = "+ if else 123";
-
-    Token token;
-    while ((token = getNextToken(input)) != Token::EOF) {
-        std::cout << "Token: " << token << std::endl;
-    }
-
+    std::string inputToken;
+    std::cout << "Digite o token: ";
+    std::cin >> inputToken;
+    
+    Token token = getToken(inputToken);
+    
+    std::cout << "Token encontrado: " << token << std::endl;
+    
     return 0;
 }
